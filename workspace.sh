d373d4993e0667486c71a45ce3c6d33e25903d8f
orzv <orzv@outlook.com>
2022-02-01 19:08:27 +0800

    Use simple zshrc config, remove ohmyzsh and starship

--- workspace.sh
+++ workspace.sh
@@ -25,33 +25,17 @@ gunzip clash-linux-amd64-v1.9.0.gz
 sudo mv clash-linux-amd64-v1.9.0 /opt/bin/clash
 sudo chmod +x /opt/bin/clash
 
-# 下载装逼神器starship
-curl -fsSLO https://mirror.ghproxy.com/github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-musl.tar.gz
-tar -xzf starship-x86_64-unknown-linux-musl.tar.gz
-sudo mv starship /opt/bin/starship
-sudo chmod +x /opt/bin/starship
-rm starship-x86_64-unknown-linux-musl.tar.gz
-
 # 下载deno
 curl -fsSL https://x.deno.js.cn/install.sh | sudo DENO_INSTALL=/opt sh
 
-# 安装oh-my-zsh
-curl -fssLO https://gitee.com/mirrors/oh-my-zsh/raw/master/tools/install.sh
-
-# 切换国内镜像
-sed -i 's/-ohmyzsh/-mirrors/' install.sh
-sed -i 's/-https:\/\/github.com/-https:\/\/gitee.com/' install.sh
-chmod +x install.sh
-
-./install.sh --unattended
-rm install.sh
+# 下载zshrc
+curl -fsSL -o myzshrc https://cdn.jsdelivr.net/gh/orzv/workspace.sh/zshrc
 
 # 添加其他配置
-sed -i '2a export PATH=/opt/bin:$PATH' ~/.zshrc
-sed -i '2a export PATH=$HOME/.deno/bin:$PATH' ~/.zshrc
-echo 'alias vi=nvim' >> ~/.zshrc
-echo 'alias ds=du -sh' >> ~/.zshrc
-echo 'eval "$(starship init zsh)"' >> ~/.zshrc
+echo 'source ~/myzshrc' >> ~/.zshrc
+echo 'export PATH=$HOME/.deno/bin:$PATH' >> ~/.zshrc
+
+# 修改ssh默认端口
 sudo chmod 777 /etc/ssh/sshd_config
 sudo echo 'Port 10022' >> /etc/ssh/sshd_config
 sudo chmod 644 /etc/ssh/sshd_config
---
+++ zshrc
@@ -0,0 +1,28 @@
+setopt autocd
+setopt nocaseglob
+setopt sharehistory
+setopt appendhistory
+setopt correct
+setopt correctall
+
+export PATH=/opt/bin:$PATH
+export PATH=/opt/nvim/bin:$PATH
+
+[ -d ~/.funcs ] && export FPATH=~/.funcs:$FPATH
+
+alias -g ll='ls -lahG'
+alias -g l='ls -lAhG'
+alias -g vi='nvim'
+[ -d ~/Desktop ] && alias d='cd ~/Desktop'
+alias ds='du -sh'
+
+autoload -Uz compinit && compinit
+autoload -Uz colors && colors
+autoload gmt
+
+zstyle ':completion:*' menu select
+
+EDITOR=nvim
+PROMPT='%(?.%F{green}√.%F{red}?%?)%f %B%F{cyan}%3~%f%b %F{magenta}%#%f '
+
+# eval "$(deno completions zsh)"
